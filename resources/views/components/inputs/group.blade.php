2a8129f9a67ea5b569e006a0ddc53f115617a2db
MarkasCherry <markas.cherry@gmail.com>
2021-05-31 02:39:46 +0200

    Updated group component

--- resources/views/components/inputs/group.blade.php
+++ resources/views/components/inputs/group.blade.php
@@ -1,17 +1,12 @@
 <div class="field">
-    <x-jet-label for="{{ $attributes->get('for') }}" important="{{ $attributes->get('important') }}" value="{{ $title }}" />
+    <x-jet-label for="{{ $attributes->get('for') }}" value="{{ $title }}"/>
     <input id="{{ $attributes->get('for') }}"
            type="{{ $attributes->get('type', 'text') }}"
-           @if($attributes->get('model', false))
-                wire:model.defer="{{ $attributes->get('model') ?? $attributes->get('name', '') }}"
-           @else
-               name="{{ $attributes->get('name') ?? '' }}"
-               value="{{ old($attributes->get('for') ?? '', $attributes->get('value') ?? '') }}"
-           @endif
-           @if($attributes->get('placeholder'))
-                placeholder="{{ $attributes->get('placeholder') }}"
-           @endif
+           @if($attributes->get('model', true)) wire:model.{{ $attributes->get('update', 'defer') }}="{{ $attributes->get('model') ?? $attributes->get('name', '') }}" @endif
+           name="{{ $attributes->get('name') ?? '' }}"
+           value="{{ old($attributes->get('for') ?? '', $attributes->get('value') ?? '') }}"
            autocomplete="{{ $attributes->get('autocomplete') ?? $attributes->get('for') ?? '' }}"
+           @if($attributes->get('readonly', false)) readonly @endif
            class="input"
     />
     <x-jet-input-error for="{{ $attributes->get('for') }}" class="mt-2"/>
